0a927103de68897e09603e7e8af4c9366d0fac4b
Glenn Spinks <glennspinks@gmail..com>
2015-06-20 22:50:33 +0800

    Upload form data

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FileUpload.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // GET: api/Upload
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Upload/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Upload
+        public HttpResponseMessage Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // PUT: api/Upload/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Upload/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UploadClient/app/app.js
@@ -0,0 +1,56 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('fileUploader', [])
+        .controller('uploadController', uploadControl);
+    //.directive('dragAndDrop', dragAndDropDirective)
+    
+    function uploadControl() {
+        var vm = this;
+        vm.pulse = ".";
+    };
+
+    //function dragAndDropDirective() {
+    //    return {
+    //        restrict: 'A',
+    //        scope: { files: '='},
+    //        link: function (scope, elem, attr) {
+    //            elem.bind('dragover', function (e) {
+    //                e.stopPropagation();
+    //                e.preventDefault();
+    //                e.dataTransfer.dropEffect = 'copy';
+    //            });
+
+    //            elem.bind('dragenter', function (e) {
+    //                e.stopPropagation();
+    //                e.preventDefault();
+    //                scope.$apply(function () {
+    //                    scope.divClass = 'on-drag-enter';
+    //                });
+    //            });
+
+    //            elem.bind('dragleave', function (e) {
+    //                e.stopPropagation();
+    //                e.preventDefault();
+    //                scope.$apply(function () {
+    //                    scope.divClass = '';
+    //                });
+    //            });
+
+    //            elem.bind('drop', function (e) {
+    //                var dropped = e.dataTransfer.files[0];
+    //                e.stopPropagation();
+    //                e.preventDefault();
+
+    //                alert(dropped.name);
+    //                scope.files.push(dropped);
+
+    //            });
+    //        }
+    //    };
+    //};
+
+    //Drag and drop directive
+    //http://jsfiddle.net/lsiv568/fsfPe/10/
+
+})();
